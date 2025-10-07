0cd4b190df626aa28acf5477677ed59f4b125db0
Baronyu188 <baron202302@163.com>
2025-10-07 19:57:09 +0800

    chore: add comment about base config removal for netlify

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,5 +4,6 @@ import { defineConfig } from 'astro/config';
 // https://astro.build/config
 export default defineConfig({
   site: 'https://foeai.netlify.app',
+  // base removed for root deployment on Netlify
   trailingSlash: 'always'
 });
